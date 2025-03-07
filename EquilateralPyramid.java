37199063a47d9cf87cafaf0b0460b59b5f2509cd
Riddhim Kawdia <riddhim10000@gmail.com>
2025-03-07 14:04:43 +0530

    Created EquilateralPyramid.java

---
+++ EquilateralPyramid.java
@@ -0,0 +1,7 @@
+class EquilateralPyramid extends Shape implements VolumeInterface
+{
+    public EquilateralPyramid(double base, double height) 
+    {
+        super("Equilateral Pyramid", base, height, 0);
+    }
+
