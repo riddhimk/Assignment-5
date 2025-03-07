1fb5d9860ae57dd483e3b94021cc5c96b9e5645b
Riddhim Kawdia <riddhim10000@gmail.com>
2025-03-07 14:02:41 +0530

    Added method to calculate sphere area

--- Sphere.java
+++ Sphere.java
@@ -5,3 +5,8 @@ class Sphere extends Shape implements VolumeInterface
         super("Sphere", radius, 0, 0);
     }
 
+    public double calculateArea() 
+    {
+        return 4 * Math.PI * dim_one * dim_one;
+    }
+
